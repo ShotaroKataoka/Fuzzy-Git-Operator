be03a9ec2d4d0d9a9afdcbd598c125bb036b2c73
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2020-12-01 03:53:51 +0900

    :tada:initial commit

---
+++ fzf_gitmoji.zsh
@@ -0,0 +1,12 @@
+
+function _fzf_gitmoji_widget() {
+  local lbuf=$LBUFFER
+  local tail=${LBUFFER:$(( ${#LBUFFER} - ${#trigger} ))}
+  local selected_moji=''
+  selected_moji=$(echo $(cat git_moji_list.txt) | sed '/^$/d' | fzf +m --ansi --cycle --bind='alt-h:abort,alt-j:down,alt-k:up,alt-l:accept')
+  selected_moji=$(echo $selected_moji | cut -f 3 -d ' ')
+  LBUFFER="$lbuf""$selected_moji""$tail"
+  zle reset-prompt
+}
+zle     -N   _fzf_gitmoji_widget
+bindkey '\eg\ec' _fzf_gitmoji_widget
