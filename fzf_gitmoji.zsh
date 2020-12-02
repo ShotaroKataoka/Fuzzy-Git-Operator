5369830d70b958012921ea05f3df55015e8304a0
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2020-12-02 10:54:41 +0900

    :sparkles:To be able to recognize .git repository; fzf_gitmoji/zsh

--- fzf_gitmoji.zsh
+++ fzf_gitmoji.zsh
@@ -1,4 +1,4 @@
-
+# Git emoji selector
 function _fzf_gitmoji_widget() {
   local lbuf=$LBUFFER
   local tail=${LBUFFER:$(( ${#LBUFFER} - ${#trigger} ))}
@@ -8,29 +8,49 @@ function _fzf_gitmoji_widget() {
   LBUFFER="$lbuf""$selected_moji""$tail"
   zle reset-prompt
 }
+
+# Git status
 function _fzf_gitstatus_widget() {
-  git status
+  local _is_git_dir=$(git rev-parse --git-dir 2> /dev/null)
+  if [ -n "$_is_git_dir" ]; then
+    git status
+    echo 
+    echo 
+  else
+    echo "Not a git repository."
+    echo 
+    echo 
+  fi
   zle reset-prompt
 }
+
+# Git log selector
 function _fzf_gitlog_widget() {
-  local _gitmoji=$(echo $(cat ~/.fzf-gitmoji-selector/git_moji_list.txt) | sed '/^$/d')
-  local _gitmoji_text=($(echo $_gitmoji | cut -f 3 -d ' '))
-  local _gitmoji_emoji=($(echo $_gitmoji | cut -f 2 -d ' '))
-  local _gitlog=$(git log --oneline)
-  end_length=10
-  for i in $(seq 1 $end_length); do
-    local text=${_gitmoji_text[$i]}
-    local emoji=${_gitmoji_emoji[$i]}
-    _gitlog=$(echo $_gitlog | sed -e "s/$text/$emoji/g")
-  done
-  _gitlog=$(echo $_gitlog | sed -e 's/^/\\033[33m/g')
-  _gitlog=$(echo $_gitlog | sed -e 's/ /\\033[0m /')
-  echo
-  local selected_commit=$(echo -e $_gitlog | fzf +m --height=70% --no-sort --ansi --cycle --bind='alt-h:abort,alt-j:down,alt-k:up,alt-l:accept,alt-c:abort,left:abort,right:accept')
-  selected_commit=$(echo $selected_commit | cut -f 1 -d ' ')
-  local lbuf=$LBUFFER
-  local tail=${LBUFFER:$(( ${#LBUFFER} - ${#trigger} ))}
-  LBUFFER="$lbuf""$selected_commit""$tail"
+  local _is_git_dir=$(git rev-parse --git-dir 2> /dev/null)
+  if [ -n "$_is_git_dir" ]; then
+    local _gitmoji=$(echo $(cat ~/.fzf-gitmoji-selector/git_moji_list.txt) | sed '/^$/d')
+    local _gitmoji_text=($(echo $_gitmoji | cut -f 3 -d ' '))
+    local _gitmoji_emoji=($(echo $_gitmoji | cut -f 2 -d ' '))
+    local _gitlog=$(git log --oneline)
+    end_length=10
+    for i in $(seq 1 $end_length); do
+      local text=${_gitmoji_text[$i]}
+      local emoji=${_gitmoji_emoji[$i]}
+      _gitlog=$(echo $_gitlog | sed -e "s/$text/$emoji/g")
+    done
+    _gitlog=$(echo $_gitlog | sed -e 's/^/\\033[33m/g')
+    _gitlog=$(echo $_gitlog | sed -e 's/ /\\033[0m /')
+    echo
+    local selected_commit=$(echo -e $_gitlog | fzf +m --height=70% --no-sort --ansi --cycle --bind='alt-h:abort,alt-j:down,alt-k:up,alt-l:accept,alt-c:abort,left:abort,right:accept')
+    selected_commit=$(echo $selected_commit | cut -f 1 -d ' ')
+    local lbuf=$LBUFFER
+    local tail=${LBUFFER:$(( ${#LBUFFER} - ${#trigger} ))}
+    LBUFFER="$lbuf""$selected_commit""$tail"
+  else
+    echo "Not a git repository."
+    echo 
+    echo 
+  fi
   zle reset-prompt
 }
 
