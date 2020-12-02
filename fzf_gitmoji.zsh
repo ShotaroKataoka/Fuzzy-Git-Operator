c4d7924d6ab771d7f08d1209c3baa508652a98c3
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2020-12-02 11:40:36 +0900

    :bug:git repository path fix

--- fzf_gitmoji.zsh
+++ fzf_gitmoji.zsh
@@ -2,8 +2,14 @@
 function _fzf_gitmoji_widget() {
   local lbuf=$LBUFFER
   local tail=${LBUFFER:$(( ${#LBUFFER} - ${#trigger} ))}
+  local _git_dir=$(git rev-parse --show-toplevel 2> /dev/null)
+  if [ -f "$_git_dir/.git_emoji_list.txt" ]; then
+    local _gitmoji=$(echo $(cat $_git_dir/.git_emoji_list.txt) | sed '/^$/d')
+  else
+    local _gitmoji=$(echo $(cat ~/.fzf-gitmoji-selector/git_emoji_list.txt) | sed '/^$/d')
+  fi
   local selected_moji=''
-  selected_moji=$(echo $(cat ~/.fzf-gitmoji-selector/git_emoji_list.txt) | sed '/^$/d' | fzf +m --ansi --cycle --bind='alt-h:abort,alt-j:down,alt-k:up,alt-l:accept,alt-c:abort,left:abort,right:accept')
+  selected_moji=$(echo $_gitmoji | fzf +m --ansi --cycle --bind='alt-h:abort,alt-j:down,alt-k:up,alt-l:accept,alt-c:abort,left:abort,right:accept')
   selected_moji=$(echo $selected_moji | cut -f 3 -d ' ')
   LBUFFER="$lbuf""$selected_moji""$tail"
   zle reset-prompt
@@ -11,8 +17,8 @@ function _fzf_gitmoji_widget() {
 
 # Git status
 function _fzf_gitstatus_widget() {
-  local _git_dir=$(git rev-parse --git-dir 2> /dev/null)
-  if [ -n "$_git_dir" ]; then
+  local _is_git_dir=$(git rev-parse --git-dir 2> /dev/null)
+  if [ -n "$_is_git_dir" ]; then
     git status
     echo 
     echo 
@@ -26,18 +32,18 @@ function _fzf_gitstatus_widget() {
 
 # Git log selector
 function _fzf_gitlog_widget() {
-  local _git_dir=$(git rev-parse --git-dir 2> /dev/null)
-  if [ -n "$_git_dir" ]; then
-    if [ -f "$_git_dir/.git_moji_list.txt" ]; then
+  local _is_git_dir=$(git rev-parse --git-dir 2> /dev/null)
+  if [ -n "$_is_git_dir" ]; then
+    local _git_dir=$(git rev-parse --show-toplevel 2> /dev/null)
+    if [ -f "$_git_dir/.git_emoji_list.txt" ]; then
       local _gitmoji=$(echo $(cat $_git_dir/.git_emoji_list.txt) | sed '/^$/d')
     else
       local _gitmoji=$(echo $(cat ~/.fzf-gitmoji-selector/git_emoji_list.txt) | sed '/^$/d')
     fi
-    local _gitmoji_text=($(echo $_gitmoji | cut -f 3 -d ' '))
     local _gitmoji_emoji=($(echo $_gitmoji | cut -f 2 -d ' '))
+    local _gitmoji_text=($(echo $_gitmoji | cut -f 3 -d ' '))
     local _gitlog=$(git log --oneline)
-    end_length=10
-    for i in $(seq 1 $end_length); do
+    for i in $(seq 1 ${#_gitmoji_text[@]}); do
       local text=${_gitmoji_text[$i]}
       local emoji=${_gitmoji_emoji[$i]}
       _gitlog=$(echo $_gitlog | sed -e "s/$text/$emoji/g")
@@ -59,7 +65,7 @@ function _fzf_gitlog_widget() {
 }
 
 zle     -N   _fzf_gitmoji_widget
-bindkey '\eg\ec' _fzf_gitmoji_widget
+bindkey '\eg\ee' _fzf_gitmoji_widget
 zle     -N   _fzf_gitstatus_widget
 bindkey '\eg\es' _fzf_gitstatus_widget
 zle     -N   _fzf_gitlog_widget
