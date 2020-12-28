39ba7657d865bdd74daceb343dd700dd98f4baa7
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2020-12-29 00:03:13 +0900

    ✨ Create F-Github-O; Add issue widget (#37)

---
+++ fuzzy_github_operator.zsh
@@ -0,0 +1,24 @@
+# Git commit 
+function _fgo_gitissue_widget() {
+  local _is_git_dir=$(git rev-parse --git-dir 2> /dev/null)
+  if [ -n "$_is_git_dir" ]; then
+    local lbuf=$LBUFFER
+    local tail=${LBUFFER:$(( ${#LBUFFER} - ${#trigger} ))}
+    local issue_list=$(gh issue list --state all --limit 10000)
+    issue_list=$(echo "$issue_list" | sed -r 's/([0-9]+)\s+OPEN/\\033[32m\#\1\\033[0m/g')
+    issue_list=$(echo "$issue_list" | sed -r 's/([0-9]+)\s+CLOSED/\\033[31m\#\1\\033[0m/g')
+    local issue_id=$(echo "$issue_list" | fzf --ansi)
+    issue_id=$(echo "$issue_id" | cut -f 1)
+    LBUFFER="$lbuf$issue_id"
+    RBUFFER=$tail
+  else
+    echo "not a git repository."
+    echo 
+    echo 
+  fi
+  zle reset-prompt
+}
+
+
+zle     -N   _fgo_gitissue_widget
+bindkey '\eg\eh\ei' _fgo_gitissue_widget
--- install.zsh
+++ install.zsh
@@ -1,6 +1,8 @@
 cp ~/.fgo/data/git_emoji_list.txt ~/.fgo/user/
 echo "source ~/.fgo/fuzzy_git_operator.zsh" >> ~/.zshrc
+echo "source ~/.fgo/fuzzy_github_operator.zsh" >> ~/.zshrc
 source ~/.fgo/fuzzy_git_operator.zsh
+source ~/.fgo/fuzzy_github_operator.zsh
 
 # install fzf
 if [ "$(which fzf)" = "fzf not found" ]; then
