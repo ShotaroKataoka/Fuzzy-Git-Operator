349d96fafa54ac657fd5f9557dd63f9c5b29ff98
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2020-12-29 12:00:03 +0900

    👍 issue selector; better viewing (#40)

--- fuzzy_github_operator.zsh
+++ fuzzy_github_operator.zsh
@@ -5,13 +5,18 @@ function _fgo_gitissue_selector() {
     local lbuf=$LBUFFER
     local tail=$RBUFFER
     local issue_list=$(gh issue list --state all --limit 10000)
-    issue_list=$(echo "$issue_list" | sed -r 's/([0-9]+)\t+OPEN/\\033[32m\#\1\\033[0m/g')
-    issue_list=$(echo "$issue_list" | sed -r 's/([0-9]+)\t+CLOSED/\\033[31m\#\1\\033[0m/g')
+    issue_list=$(echo "$issue_list" | sed -r 's/([0-9]+)\t+OPEN/\\033[32m\#\1 Open\\033[0m/g')
+    issue_list=$(echo "$issue_list" | sed -r 's/([0-9]+)\t+CLOSED/\\033[31m\#\1 Close\\033[0m/g')
+
+    issue_list=$(echo "$issue_list" | sed -r 's/\t([a-zA-Z0-9 \f\n\r\-]*)/\\033[33m\t\1\\033[0m/2')
+    issue_list=$(echo "$issue_list" | sed -r 's/\t([0-9\-]*) ?[0-9\:]* ?[0-9\+]* ?[a-zA-Z]*/\\033[34m\t\1\\033[0m/3')
 
     # issue_list=$(echo "$issue_list" | sed -r 's/([0-9]+)\t/\\033[31m\#\1\\033[0m/g')
+    issue_list=$(echo "$issue_list" | column -t -s $'\t')
 
-    local issue_id=$(echo "$issue_list" | fzf --ansi)
-    issue_id=$(echo "$issue_id" | cut -f 1)
+    local issue_id=$(echo "$issue_list" | fzf --layout=reverse --border --cycle --info='inline' --height=50% --ansi +m --bind="alt-h:abort,alt-j:down,alt-k:up,alt-l:accept,left:abort,right:accept,alt-c:abort,ctrl-h:abort,ctrl-l:accept"
+)
+    issue_id=$(echo "$issue_id" | cut -d ' ' -f 1)
     LBUFFER="$lbuf$issue_id"
     RBUFFER="$tail"
   else
