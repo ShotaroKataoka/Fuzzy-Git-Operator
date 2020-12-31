a244c3cb20840789db2ad52b3d487daca159d8f9
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2020-12-31 13:17:49 +0900

    👍 Add update.zsh (#78)

---
+++ update.zsh
@@ -0,0 +1,43 @@
+local _ANSI_COLOR_ERROR="\\033[31;1m"
+local _ANSI_COLOR_SUCCESS="\\033[32m"
+local _ANSI_COLOR_WARN="\\033[33m"
+local _ANSI_COLOR_END="\\033[0m"
+
+echo "- Start update Fuzzy-Git-Operator!"
+if [ ! -f ~/.fgo/user/git_emoji_list.txt ]; then
+  cp ~/.fgo/data/git_emoji_list.txt ~/.fgo/user/
+  if [ $(echo $?) -eq 0 ]; then
+    echo "  -$_ANSI_COLOR_SUCCESS Done$_ANSI_COLOR_END 'cp ~/.fgo/data/git_emoji_list.txt ~/.fgo/user/'"
+  else
+    echo "  -$_ANSI_COLOR_ERROR Error$_ANSI_COLOR_END 'cp ~/.fgo/data/git_emoji_list.txt ~/.fgo/user/'$_ANSI_COLOR_END"
+  fi
+else
+  echo "  -$_ANSI_COLOR_SUCCESS Already exists$_ANSI_COLOR_END '~/.fgo/user/git_emoji_list.txt'$_ANSI_COLOR_END"
+fi
+
+if [ ! -f ~/.fgo/user/color_scheme.zsh ]; then
+  cp ~/.fgo/data/color_scheme.zsh ~/.fgo/user/
+  if [ $(echo $?) -eq 0 ]; then
+    echo "  -$_ANSI_COLOR_SUCCESS Done$_ANSI_COLOR_END 'cp ~/.fgo/data/color_scheme.zsh ~/.fgo/user/'$_ANSI_COLOR_END"
+  else
+    echo "  -$_ANSI_COLOR_ERROR Error$_ANSI_COLOR_END 'cp ~/.fgo/data/git_emoji_list.txt ~/.fgo/user/'$_ANSI_COLOR_END"
+  fi
+else
+  echo "  -$_ANSI_COLOR_SUCCESS Already exists$_ANSI_COLOR_END '~/.fgo/user/color_scheme.zsh'$_ANSI_COLOR_END"
+fi
+
+if [ ! -f ~/.fgo/user/fgo_keybindings.zsh ]; then
+  cp ~/.fgo/data/fgo_keybindings.zsh ~/.fgo/user/
+  if [ $(echo $?) -eq 0 ]; then
+    echo "  -$_ANSI_COLOR_SUCCESS Done$_ANSI_COLOR_END 'cp ~/.fgo/data/fgo_keybindings.zsh ~/.fgo/user/'$_ANSI_COLOR_END"
+  else
+    echo "  -$_ANSI_COLOR_ERROR Error$_ANSI_COLOR_END 'cp ~/.fgo/data/git_emoji_list.txt ~/.fgo/user/'$_ANSI_COLOR_END"
+  fi
+else
+  echo "  -$_ANSI_COLOR_SUCCESS Already exists$_ANSI_COLOR_END '~/.fgo/user/fgo_keybindings.zsh'$_ANSI_COLOR_END"
+fi
+source ~/.fgo/fgo.zsh
+
+echo "$_ANSI_COLOR_SUCCESS Update complete.$_ANSI_COLOR_END"
+echo "If you want to update above files, try $_ANSI_COLOR_WARN'cp ~/.fgo/data/<file> ~/.fgo/user/'$_ANSI_COLOR_END."
+
